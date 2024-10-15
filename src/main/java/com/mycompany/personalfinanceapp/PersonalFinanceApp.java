538e6665ec9c4841d0ae1dc738c71454b5ae7f53
RicardoRG96 <ricardoretamal10@gmail.com>
2024-10-15 06:05:13 -0300

    first commit

---
+++ src/main/java/com/mycompany/personalfinanceapp/PersonalFinanceApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.personalfinanceapp;
+
+/**
+ *
+ * @author ricar
+ */
+public class PersonalFinanceApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
